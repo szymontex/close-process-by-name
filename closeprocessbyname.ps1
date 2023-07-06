9383d6c8851de716c765c01b97c7b402a6d59bd4
Szymon Gwóźdź <76672545+szymontex@users.noreply.github.com>
2023-07-06 03:28:34 +0200

    Update closeprocessbyname.ps1

--- closeprocessbyname.ps1
+++ closeprocessbyname.ps1
@@ -1,6 +1,6 @@
-C:\Users\ST2> $getget = gwmi win32_process | select Handle, CommandLine
-C:\Users\ST2> $getpid = $getget | Where-Object {$_.CommandLine -match "INSERT_HERE"} | select -ExpandProperty Handle
-C:\Users\ST2> foreach ($process in $getpid)
->> {
->>     Stop-Process -Id $process
->> }
+$getprocesses = gwmi win32_process | select Handle, CommandLine
+$getpid = $getprocesses | Where-Object {$_.CommandLine -match "INSERT_HERE"} | select -ExpandProperty Handle
+foreach ($process in $getpid)
+{
+    Stop-Process -Id $process
+}
